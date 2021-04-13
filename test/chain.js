81faf8e9975c6e1c99e7af934492eb049817a693
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2021-04-13 10:22:41 -0400

    Linting baby!

--- test/chain.js
+++ test/chain.js
@@ -1,47 +1,5 @@
-"use strict";
-//
-//! Souce of code: https://www.youtube.com/watch?v=qF7dkrce-mQ
-//
-var __createBinding =
-  (this && this.__createBinding) ||
-  (Object.create
-    ? function (o, m, k, k2) {
-        if (k2 === undefined) k2 = k;
-        Object.defineProperty(o, k2, {
-          enumerable: true,
-          get: function () {
-            return m[k];
-          },
-        });
-      }
-    : function (o, m, k, k2) {
-        if (k2 === undefined) k2 = k;
-        o[k2] = m[k];
-      });
-var __setModuleDefault =
-  (this && this.__setModuleDefault) ||
-  (Object.create
-    ? function (o, v) {
-        Object.defineProperty(o, "default", { enumerable: true, value: v });
-      }
-    : function (o, v) {
-        o["default"] = v;
-      });
-var __importStar =
-  (this && this.__importStar) ||
-  function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null)
-      for (var k in mod)
-        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k))
-          __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-  };
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto = __importStar(require("crypto"));
-// Transfer of funds between two walletime
+const crypto = require("crypto");
+// Transfer of funds between two wallets
 class Transaction {
   constructor(
     amount,
